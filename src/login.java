883a6cf4ea93517f3d5c16d8b0cde52ccdc29318
Wildan <Wildan@Wildan-PC>
2017-03-22 21:38:18 +0700

    CRD&Print Done

--- src/login.java
+++ src/login.java
@@ -3,7 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
 /**
  *
  * @author Wildan
@@ -26,21 +32,131 @@ public class login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel2 = new javax.swing.JLabel();
+        txtnama = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        txtpass = new javax.swing.JTextField();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel3 = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(null);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        jLabel2.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("Username");
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(0, 70, 400, 46);
 
-        pack();
+        txtnama.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtnamaActionPerformed(evt);
+            }
+        });
+        getContentPane().add(txtnama);
+        txtnama.setBounds(100, 110, 210, 30);
+
+        jLabel1.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Password");
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(0, 130, 400, 46);
+        getContentPane().add(txtpass);
+        txtpass.setBounds(100, 170, 210, 30);
+
+        jButton2.setText("Sign Up");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton2);
+        jButton2.setBounds(79, 230, 80, 30);
+
+        jButton3.setText("Exit");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton3);
+        jButton3.setBounds(170, 230, 73, 30);
+
+        jButton1.setText("Sign In");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1);
+        jButton1.setBounds(250, 230, 80, 30);
+
+        jPanel1.setBackground(new java.awt.Color(0, 29, 82));
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setText("Rental Motor");
+        jPanel1.add(jLabel3);
+
+        getContentPane().add(jPanel1);
+        jPanel1.setBounds(0, 0, 400, 50);
+
+        setSize(new java.awt.Dimension(416, 339));
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void txtnamaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtnamaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtnamaActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        String username = txtnama.getText();
+        String password = txtpass.getText();
+        
+        if(username.isEmpty() || password.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Mohon maaf, harap isi dengan benar");
+        } else {
+            try{
+                try (Statement statement = (Statement) file_koneksi.GetConnection().createStatement()) {
+                    statement.executeUpdate("INSERT INTO `admin`(`username`,`password`) VALUES ('"+username+"','"+password+"')");
+                }
+                JOptionPane.showMessageDialog(null,"Selamat ! Anda berhasil Sign Up !");
+            } catch (Exception t){
+                JOptionPane.showMessageDialog(null, "Mohon maaf, ulangi lagi prosedur");
+            }
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        this.setVisible(false);
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        Connection connection;
+        PreparedStatement ps;
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/pbo_rental?zeroDateTimeBehavior=convertToNull","root","");
+            ps = connection.prepareStatement("SELECT `username`,`password` FROM `admin` WHERE `username` = ? AND `password` = ?");
+            ps.setString(1, txtnama.getText());
+            ps.setString(2, txtpass.getText());
+            ResultSet result = ps.executeQuery();
+            if(result.next()){
+                new Main().show();
+                this.dispose();
+            }
+            else {
+                JOptionPane.showMessageDialog(rootPane, "Salah !");
+                txtpass.setText("");
+                txtnama.requestFocus();
+            }
+        } catch (SQLException ex){
+            System.out.println(ex.toString());
+            JOptionPane.showMessageDialog(rootPane, "gagal");
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +194,14 @@ public class login extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField txtnama;
+    private javax.swing.JTextField txtpass;
     // End of variables declaration//GEN-END:variables
 }
